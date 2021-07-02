67eb891b746587d459c718c7bedf9ff717da294f
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-02 16:45:15 +0530

    Create HackerRank-Tree-Level-Order-Traversel.java

---
+++ Day3/HackerRank-Tree-Level-Order-Traversel.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static void levelOrder(Node root) {
+        Queue<Node> queue=new LinkedList<Node>();
+        Node temp=root;
+        queue.add(temp);
+        while(!queue.isEmpty()){
+            temp=queue.peek();
+            queue.remove();
+            System.out.print(temp.data+" ");
+            if(temp.left!=null){
+                queue.add(temp.left);
+            }
+            if(temp.right!=null){
+                queue.add(temp.right);
+            }
+        }
+      
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}

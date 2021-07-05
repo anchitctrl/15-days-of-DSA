374224f2f8da35748ddaf33ab7f8813754470197
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-05 13:47:07 +0530

    Create HackerRank-Inserting-a-Node-into-a-Sorted-Doubly-Linked-List.java

---
+++ Day6/HackerRank-Inserting-a-Node-into-a-Sorted-Doubly-Linked-List.java
@@ -0,0 +1,127 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
+        DoublyLinkedListNode newNode=new DoublyLinkedListNode(data);
+        DoublyLinkedListNode trial=llist;
+        while(trial.next!=null){
+            trial=trial.next;
+        }
+        DoublyLinkedListNode tail=trial;
+        if(tail.data<data){
+            tail.next=newNode;
+            newNode.next=null;
+            newNode.prev=tail;
+            tail=newNode;
+            return llist;   
+        }
+        if(llist.data>data){
+            newNode.next=llist;
+            llist.prev=newNode;
+            newNode.prev=null;
+            llist=newNode;
+            return llist;
+        }
+        DoublyLinkedListNode current=llist;
+        while(current.next.data<data && current.next!=null){
+            current=current.next;
+        }
+        DoublyLinkedListNode temp=current.next;
+        current.next=newNode;
+        newNode.next=temp;
+        temp.prev=newNode;
+        newNode.prev=current;
+        return llist;
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            int data = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

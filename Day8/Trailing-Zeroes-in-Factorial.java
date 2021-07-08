cd1c15deefb9580c2ba57280d2829bc7eba75909
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-08 17:10:06 +0530

    Create Trailing-Zeroes-in-Factorial.java

---
+++ Day8/Trailing-Zeroes-in-Factorial.java
@@ -0,0 +1,8 @@
+class Solution{
+    static int trailingZeroes(int N){
+        int count=0;
+        for (int i = 5; N / i >= 1; i *= 5)
+            count += N / i;
+        return count;
+    }
+}

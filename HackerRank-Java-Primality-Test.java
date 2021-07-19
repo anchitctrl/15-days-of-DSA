8b7697183b56e933b6e77b51d21b9bd2aba15e92
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-19 13:31:53 +0530

    Create HackerRank-Java-Primality-Test.java

---
+++ HackerRank-Java-Primality-Test.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        try (Scanner scanner = new Scanner(System.in);){
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}

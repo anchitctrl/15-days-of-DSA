bf4bc821e376e8d1b0244ea8faa637569fcf2857
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-28 13:29:42 +0530

    Create HackerRank-FizzBuzz.java

---
+++ HackerRank-FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");   
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
